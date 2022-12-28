44d1519b5c57123c13223e0c5cac75ecba29594e
kyunghwaisdev <kyunghwaisdev@gmail.com>
2022-12-29 08:59:11 +0900

    Initial commit

---
+++ src/main/java/com/example/board/dto/BoardRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.board.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BoardRequestDto {
+    private String title;
+    private String content;
+    private String author;
+    private String password;
+}
